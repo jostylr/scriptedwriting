7013cc2a843043baef41a9317e6da570b2d8135a
James Taylor <jostylr@gmail.com>
2012-03-03 17:38:25 -0500

    fixed closure issues and added inline clicking.

--- scriptedwriting.js
+++ scriptedwriting.js
@@ -18,9 +18,12 @@
       } else if (self.hasClass("click") ) { //code snippet with class click
           //click to run
           text = self.text();
-          self.click(function () {
-            eval(text);
-          });
+          self.click((function () {
+            var t = text;
+            return function () {
+              eval(t);
+            }; 
+          } () )  );
           self.addClass("javascript").removeClass("click");
       }
       else if (self.attr("href") && self.text().match(runreg)) { //link
@@ -28,27 +31,39 @@
         $.ajax({
           url: url,
           dataType : "script",
-          success: function () {
-            self.hide();
-          }
+          success: (function () {
+            var s = self;
+            return function () {
+              s.hide();
+          };
+        } () )
         });
       }  else if (self.attr("href") && self.text().match(clickreg)) { //link
           url = self.attr("href"); //
           $.ajax({
             url: url,
             dataType : "text",
-            success: function (data) {
-              var chunk;
-              chunk = data.split("<!--split-->")[1];
-              self.replaceWith(chunk);
-              $.ajax({
-                url : url.replace(".html", ".js"),
-                dataType: "script"
-              });
-            }
+            success: (function () {
+              var s = self;
+              return function (data) {
+                var chunk;
+                chunk = data.split("<!--split-->")[1];
+                s.replaceWith(chunk);
+                $.ajax({
+                  url : url.replace(".html", ".js"),
+                  dataType: "script"
+                });
+              };
+            } () )
           });
-      } else if (self.text().match(runreg) ) {
-        eval(self.text().replace(runreg, '') );
+      } else if (self.text().match(clickreg) ) {
+        text = self.text().replace(clickreg, '');
+        self.click((function () {
+          var t = text;
+          return function () {
+            eval(t);
+          };} ())
+        );
       }
     });
   };
